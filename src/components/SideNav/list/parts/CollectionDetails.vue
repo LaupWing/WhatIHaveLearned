091647edb904073aec0cfe7ff28fbf0f5c527021
LaupWing <loc_strict@hotmail.com>
2019-10-30 19:50:44 +0100

    img checker added

--- src/components/SideNav/list/parts/CollectionDetails.vue
+++ src/components/SideNav/list/parts/CollectionDetails.vue
@@ -6,7 +6,7 @@
 
 <script>
 import {mapGetters, mapActions} from 'vuex'
-
+import capatalize from '@/helpers/string'
 export default {
     name: 'CollectionDetails',
     props:['collection'],
@@ -15,19 +15,29 @@ export default {
     },
     data(){
         return{
-            defaultDisplay: `<img src="${this.collection.icon.src}" style="display: block; margin: auto;" width="50">
-            <h1><span class="ql-size-large" style="color: rgb(255, 255, 255); font-size: 48px; margin:auto;">${this.collection.collection}</span></h1>`
+            defaultDisplay: `
+                <p class="ql-align-center">${this.checkTypeIcon()}</p><p class="ql-align-center"></p><h1 class="ql-align-center"><span style="color: rgb(255, 255, 255); font-size: 48px;">${capatalize(this.collection.collection)}</span></h1><p><br></p>
+            `
         }
     },
     methods:{
         ...mapActions(['setMainContent']),
+        checkTypeIcon(){
+            console.log('checking type icon', this.collection.type)
+            if(this.collection.icon.type === 'img'){
+                console.log(this.collection, 'img')
+                return `<img src="${this.collection.icon.src}" width="100" style="display: block; margin: auto;">`
+            }else if(this.collection.icon.type === 'svg'){
+                console.log(this.collection.icon.src, 'svg')
+                return this.collection.icon.src
+            }
+        }
     },
     created(){
         if(!this.collection.introduction){
             this.setMainContent(this.defaultDisplay)
         }
-        console.log(this.collection)
-        // this.setMainContent()
+        console.log(this.defaultDisplay)
     }
 }
 </script>
