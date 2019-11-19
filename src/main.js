916dc05b1b2b9cde309a48054a6f3728ccf942fe
LaupWing <loc_strict@hotmail.com>
2019-11-19 12:16:12 +0100

    added initial loading when state is loaded

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import firebase from 'firebase'
 import store from './store'
-
 Vue.config.productionTip = false
 
+
 firebase.auth().onAuthStateChanged(user=>{
-  new Vue({
-        store,
-        render: h => h(App),
-    }).$mount('#app')
+    console.log(user)
+    firebase.auth().onAuthStateChanged(user=>{
+      new Vue({
+            store,
+            render: h => h(App),
+        }).$mount('#app')
+    })
 })
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
 import collections from './modules/collections'
+import firebase from 'firebase'
 
 // Load Vuex
 
