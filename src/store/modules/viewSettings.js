aab7721cb12d578167870a6d3560c51773dc643c
LaupWing <loc_strict@hotmail.com>
2019-11-26 15:09:55 +0100

    added the basic viewSettings store

---
+++ src/store/modules/viewSettings.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase'
+import db from '@/firebase/init'
+
+const state = {
+    
+}
+
+const getters = {
+}
+
+const actions = {
+}
+
+const mutations = {
+    
+}
+
+export default{
+    state,
+    actions,
+    getters,
+    mutations
+}
